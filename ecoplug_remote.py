7cfe00940acdabad1480e3aee4a522da3ee87d34
montymagic9 <calebluke01@gmail.com>
2019-03-19 21:40:36 +0000

    Add files via upload

---
+++ ecoplug_remote.py
@@ -0,0 +1,38 @@
+import serial
+from datetime import datetime
+import socket
+
+# Start a serial connection on port 'COM3' with a baud rate of 115200
+ser1 = serial.Serial('COM3', 115200)
+
+# Opens a server with the device's ip and listens for incoming data
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('192.168.43.94', 1339))
+s.listen(1)
+
+# Accept's any devices trying to connect
+print('listening..')
+conn, addr = s.accept()
+print('connected')
+
+# Used for decoding twoscomplement data which is sent over networks
+def twoscomplement_to_unsigned(i):
+    return i % 256
+
+# Loop forever, receiving data from connected clients.
+# If string 's' is received then send another 's' to the Arduino
+# over serial connection defined above.
+while True:
+    try:
+        data = conn.recv(3000)
+        result = bytes(map(twoscomplement_to_unsigned, data))
+        print('decoded: ', result)
+        message = data.decode('utf-8')
+        print(message)
+        if 's' in message:
+            print('next')
+            ser1.write('s'.encode())
+            print('SENT')
+    except:
+        print('error')
+        pass
